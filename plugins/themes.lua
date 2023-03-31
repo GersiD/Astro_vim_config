7e350b6255a0cbd448800ac47d221ed6f095e609
git <gd1064@wildcats.unh.edu>
2023-03-30 21:14:21 -0400

    make which-key lazy load

--- plugins/themes.lua
+++ plugins/themes.lua
@@ -45,6 +45,7 @@ return {
       -- style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
       -- terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
       -- no_italic = true, -- Disable italic comments, keywords, etc.
+      -- transparent = true, -- Enable transparent background
       styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
